26de1132728e5454d69e5d8362e508d3c2caafa5
Devin Lane <devin.c.lane@gmail.com>
2024-04-30 14:24:22 -0700

    fetch billboards from admin

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,20 @@
-const HomePage = () => {
-    return <div>Hello Store</div>;
+import getBillboard from "@/actions/get-billboards";
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const billboard = await getBillboard(
+        "60a04cbb-b9f5-4ddd-a88f-2a8a56ed1164"
+    );
+    return (
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard} />
+            </div>
+        </Container>
+    );
 };
 
 export default HomePage;
